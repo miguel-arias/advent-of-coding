abb39051e67a051f192de6ddbadc605a8f510bca
Miguel Arias <Miguel.Arias@jpl.nasa.gov>
2015-12-08 16:05:21 -0800

    day 8 part 2 solution, also pretty easy

--- advent8.py
+++ advent8.py
@@ -8,6 +8,7 @@ def main(argv):
     puzzle_in = open(argv[0], 'r').read().split('\n')[:-1]
     code_char = 0
     literal_char = 0
+    encode_char = 0 #part 2
 
     for item in puzzle_in:
 	code_char += len(item) #this is the easy part
@@ -16,8 +17,28 @@ def main(argv):
     print "Characters of code: %s" % code_char
     print "Characters of literal chars: %s" % literal_char
     difference = code_char - literal_char
-    print "Difference: %s" % difference
+    print "Part 1 Difference: %s" % difference
 
     
+    #part 2
+
+    for item in puzzle_in:
+	encode_string = '"' #start off with a double quote
+	for char in item:
+	    if char == '"':
+		encode_string += '\\"'
+	    elif char == '\\':
+		encode_string += '\\\\' #add an extra escape char
+	    else:
+		encode_string += char
+	encode_string += '"' #close the double quote
+#	print item
+#	print encode_string
+	encode_char += len(encode_string)
+
+    print "Characters of encode chars: %s" % encode_char
+    difference2 = encode_char - code_char
+    print "Part 2 Difference: %s" % difference2	
+
 if __name__ == "__main__":
     main(sys.argv[1:])
